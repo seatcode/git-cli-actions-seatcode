89718dc4165c862bb4bea947bf41806555e70b32
juan-fgr <juan.gonzalez@code.seat>
2022-11-22 12:14:59 +0000

    GITHUB - Updated code to manage dayjs

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,10 +2,11 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import {time} from '../src/action/time'
 
 test('wait 500 ms', async () => {
-  var delta = Math.abs(500 - 50)
-  expect(delta).toBeGreaterThan(449)
+  await time()
+  console.log('demo')
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
--- src/action/time.ts
+++ src/action/time.ts
@@ -1,14 +1,17 @@
 import * as core from '@actions/core'
 import dayjs from 'dayjs'
 
+import dayjsPluginUTC from 'dayjs/plugin/utc'
+dayjs.extend(dayjsPluginUTC)
+
 export async function time(): Promise<void> {
   const timezone = core.getInput('timeZone') // default: 0
   const formatStr = core.getInput('format') // default: ''
-  const str = dayjs(new Date(), formatStr, timezone).utcOffset()
-  /**
-   *   console.log('time zone: ', timezone)
-   *   console.log('time format: ', formatStr)
-   *   console.log('time formatStr: ', str)
-   */
+  const str = dayjs().utcOffset(timezone).format(formatStr)
+
+  console.log('time zone: ', timezone)
+  console.log('time format: ', formatStr)
+  console.log('time formatStr: ', str)
+
   core.setOutput('time', str)
 }
